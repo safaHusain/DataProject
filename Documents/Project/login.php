bab74762b787fecba619fad74465d8891e18d807
safaHusain <safa.husain07@gmail.com>
2023-05-10 18:57:11 +0300

    registration and login forms created

---
+++ Documents/Project/login.php
@@ -0,0 +1,20 @@
+<?php
+include 'header.html';
+?>
+
+<div id="main">
+  <div class="wrapper">
+    <h2>Login</h2>
+    <form action="#">
+      <div class="input-box">
+        <input type="text" placeholder="Enter your username" required>
+      </div>
+      <div class="input-box">
+        <input type="password" placeholder="Enter your password" required>
+      </div>
+      <div class="input-box button">
+        <input type="Submit" value="Login">
+      </div>
+    </form>
+  </div>
+</div>
---
+++ Documents/Project/register.php
@@ -0,0 +1,25 @@
+<?php
+include 'header.html';
+?>
+<div id="main">
+  <div class="wrapper">
+    <h2>Registration</h2>
+    <form action="#">
+      <div class="input-box">
+        <input type="text" placeholder="Enter your username" required>
+      </div>
+      <div class="input-box">
+        <input type="text" placeholder="Enter your email" required>
+      </div>
+      <div class="input-box">
+        <input type="password" placeholder="Create password" required>
+      </div>
+      <div class="input-box button">
+        <input type="Submit" value="Register Now">
+      </div>
+      <div class="text">
+        <h3>Already have an account? <a href="login.php">Login now</a></h3>
+      </div>
+    </form>
+  </div>
+</div>
