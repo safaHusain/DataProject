884afb77554707873c062c1d9c034f58017c559e
ALi Moosa <201902206@student.polytechnic.bh>
2023-05-13 15:52:21 +0300

    init commit

--- Documents/Project/news.php
+++ Documents/Project/news.php
@@ -1,7 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>News</title>
+    <!--awsome font-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!--google font-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600&display=swap" rel="stylesheet">
+    <!--style sheet-->
+    <link rel="stylesheet" href="./style.css" />
+
+</head>
+
+<body>
+    <header>
+        <img src="" width="1220px" />
+
+        <div="inputSearch">
+
+            <input type="text" placeholder="Search" class="" />
+            <!--style sheet-->
+            <span> <i class="fa-solid fa-search"></i> </span>
+            </div>
+            <nav>
+                <ul>
+                    <li>one</li>
+                    <li>two</li>
+                    <li>three</li>
+                </ul>
+            </nav>
+    </header>
+</body>
+
+</html>
+
+
 <?php
-
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
-
+?>
